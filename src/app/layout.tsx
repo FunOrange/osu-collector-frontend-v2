2ac66eb3e1239491b9dfd3e7fab7d0b0bdd19132
Jun Villa <jun@aucto.com>
2023-12-01 08:51:47 -0500

    glass navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,8 @@ import NavbarSearch from "@/components/NavbarSearch";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const navbarHeight = "64px";
+const navbarHeight = "h-14";
+const navbarSpacer = "pt-14";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,65 +34,76 @@ const navItems = [
   { label: "Users", href: "/users" },
 ];
 
+const glass = {
+  background: "rgba(30, 41, 59, 0.90)",
+  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+  backdropFilter: "blur(7.5px)",
+  border: "1px solid rgba(30, 41, 59, 1)",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="fixed top-0 z-10 flex flex-col w-full shadow-xl bg-slate-800">
-          <div className="flex justify-between w-full px-1 md:px-4 md:pr-2 h-14">
-            <div className="flex items-center">
-              <div className="block md:hidden">
-                <AlertDialog>
-                  <AlertDialogTrigger asChild>
-                    <button className="px-2 py-1 text-sm transition rounded hover:bg-slate-600">
-                      <List size={30} />
-                    </button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>osu!Collector</AlertDialogTitle>
-                    </AlertDialogHeader>
-                    <div className="flex flex-col gap-1">
-                      {[{ href: "/", label: "Home" }, ...navItems].map(({ label, href }, i) => (
-                        <Link href={href} key={i}>
-                          <AlertDialogAction
-                            className={cn(buttonVariants({ variant: "secondary" }), "w-full")}
-                          >
-                            {label}
-                          </AlertDialogAction>
-                        </Link>
-                      ))}
-                    </div>
-                    <AlertDialogCancel>Close</AlertDialogCancel>
-                  </AlertDialogContent>
-                </AlertDialog>
+        <div
+          className={cn(
+            "fixed top-0 z-10 flex flex-colshadow-xl justify-between w-full px-1 md:px-4 md:pr-2",
+            navbarHeight
+          )}
+          style={glass}
+        >
+          <div className="flex items-center">
+            <div className="block md:hidden">
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <button className="px-2 py-1 text-sm transition rounded hover:bg-slate-600">
+                    <List size={30} />
+                  </button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>osu!Collector</AlertDialogTitle>
+                  </AlertDialogHeader>
+                  <div className="flex flex-col gap-1">
+                    {[{ href: "/", label: "Home" }, ...navItems].map(({ label, href }, i) => (
+                      <Link href={href} key={i}>
+                        <AlertDialogAction
+                          className={cn(buttonVariants({ variant: "secondary" }), "w-full")}
+                        >
+                          {label}
+                        </AlertDialogAction>
+                      </Link>
+                    ))}
+                  </div>
+                  <AlertDialogCancel>Close</AlertDialogCancel>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+            <Link href="/">
+              <div className="flex items-center px-2 py-3 cursor-pointer">
+                osu!
+                <span className="font-semibold text-gray-50">Collector</span>
               </div>
-              <Link href="/">
-                <div className="flex items-center px-2 py-3 cursor-pointer">
-                  osu!
-                  <span className="font-semibold text-gray-50">Collector</span>
-                </div>
-              </Link>
-              <nav className="items-center hidden mx-6 space-x-4 lg:space-x-6 md:flex">
-                {navItems.map(({ label, href }, i) => (
-                  <Link
-                    href={href}
-                    className="text-sm font-medium transition-colors hover:text-primary"
-                    key={i}
-                  >
-                    {label}
-                  </Link>
-                ))}
-              </nav>
-            </div>
+            </Link>
+            <nav className="items-center hidden mx-6 space-x-4 lg:space-x-6 md:flex">
+              {navItems.map(({ label, href }, i) => (
+                <Link
+                  href={href}
+                  className="text-sm font-medium transition-colors hover:text-primary"
+                  key={i}
+                >
+                  {label}
+                </Link>
+              ))}
+            </nav>
+          </div>
 
-            <div className="flex items-center gap-2">
-              <NavbarSearch />
-              <UserNav />
-            </div>
+          <div className="flex items-center gap-2">
+            <NavbarSearch />
+            <UserNav />
           </div>
         </div>
-        <div style={{ paddingTop: navbarHeight }}>{children}</div>
+        <div className={navbarSpacer}>{children}</div>
       </body>
     </html>
   );
