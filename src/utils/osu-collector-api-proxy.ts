b52e3fa7c8559834eee53d3028f793913e769e7d
Jun Villa <jun@aucto.com>
2025-01-06 10:52:02 -0500

    fix: handle api host with port

--- src/utils/osu-collector-api-proxy.ts
+++ src/utils/osu-collector-api-proxy.ts
@@ -2,21 +2,27 @@ export async function proxy(method: string, req: Request) {
   const upstreamUrl = new URL(req.url);
   upstreamUrl.hostname = withoutProtocol(process.env.NEXT_PUBLIC_OSU_COLLECTOR_API_HOST);
   upstreamUrl.protocol = protocol(process.env.NEXT_PUBLIC_OSU_COLLECTOR_API_HOST);
-  upstreamUrl.port = '';
-  const upstreamResponse = await fetch(upstreamUrl.toString(), {
-    method,
-    headers: removeHeader(req.headers, 'accept-encoding'),
-  });
-  return new Response(upstreamResponse.body, {
-    status: upstreamResponse.status,
-    headers: (() => {
-      const a = removeHeader(upstreamResponse.headers, 'content-encoding');
-      return overwriteSetCookieDomain(a, upstreamUrl.hostname);
-    })(),
-  });
+  upstreamUrl.port = port(process.env.NEXT_PUBLIC_OSU_COLLECTOR_API_HOST);
+  try {
+    const upstreamResponse = await fetch(upstreamUrl.toString(), {
+      method,
+      headers: removeHeader(req.headers, 'accept-encoding'),
+    });
+    return new Response(upstreamResponse.body, {
+      status: upstreamResponse.status,
+      headers: (() => {
+        const a = removeHeader(upstreamResponse.headers, 'content-encoding');
+        return overwriteSetCookieDomain(a, upstreamUrl.hostname);
+      })(),
+    });
+  } catch (error) {
+    console.error(upstreamUrl.toString(), error);
+    return new Response(error.message, { status: 500 });
+  }
 }
 
 const protocol = (str: string) => str.match(/^https?:\/\//)?.[0] ?? 'https://';
+const port = (str: string) => str.match(/:(\d+)$/)?.[1] ?? '';
 const withoutProtocol = (str: string) => str.replace(/^https?:\/\//, '');
 
 function removeHeader(headers: Headers, headerToRemove: string) {
