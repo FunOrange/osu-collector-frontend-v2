bfb2f1f439141708fee8f8fd447d90b3aa71edaf
Jun Villa <jun@aucto.com>
2025-02-28 17:23:34 -0500

    fix inconsistent styling

--- src/app/users/[userId]/favourites/collections/page.tsx
+++ src/app/users/[userId]/favourites/collections/page.tsx
@@ -37,8 +37,8 @@ export default async function UserFavouriteCollectionsPage({ params }: PageProps
 
   return (
     <div className='flex justify-center w-full'>
-      <div className='px-2 py-5 md:px-10 max-w-screen-2xl'>
-        <div className='p-4 mb-4 rounded bg-slate-700 md:p-7'>
+      <div className='px-2 py-5 md:px-10 w-full max-w-screen-2xl'>
+        <div className='p-4 mb-4 rounded border-slate-900 shadow-inner bg-[#162032] md:p-7'>
           <h2 className='flex items-center gap-3 mb-6'>
             <Avatar className='w-10 h-10'>
               <AvatarImage src={pageUser.osuweb.avatar_url} alt='@shadcn' />
@@ -54,10 +54,10 @@ export default async function UserFavouriteCollectionsPage({ params }: PageProps
               </a>
             </div>
           </h2>
-          <h1 className='mb-2 text-xl'>
+          <h1 className='mb-2 text-2xl'>
             {collections.length === 1
               ? `${pageUser.osuweb.username} favourited this collection:`
-              : `${pageUser.osuweb.username} favourited these ${collections.length} collections`}
+              : `${pageUser.osuweb.username} favourited these ${collections.length} collections:`}
           </h1>
           <div className='grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {!collections ? (
