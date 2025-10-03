487c486131bf9bbeb904dec0f846092a8d1b0ca8
Jun Villa <jun@aucto.com>
2025-10-03 22:52:44 +0900

    fix: remove empty layout, pad otp with zeros

--- src/app/(browser)/login/showOtp/page.tsx
+++ src/app/(browser)/login/showOtp/page.tsx
@@ -1,11 +1,11 @@
 export default function EnterOtpPage({ searchParams }) {
-  const otp = searchParams.otp;
+  const otp = searchParams.otp as string;
 
   return (
     <div className='flex items-center justify-center w-full h-[90vh]'>
       <div className='flex flex-col items-center gap-4 p-12 rounded bg-slate-700'>
         <div className='text-xl'>Your one time password is:</div>
-        <div className='text-5xl'>{otp}</div>
+        <div className='text-5xl'>{otp.padStart(4, '0')}</div>
         <div className='text text-slate-400'>Please enter this code in the desktop app to login</div>
       </div>
     </div>
